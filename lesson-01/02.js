b0e60b668f9da8040efcd58c0567fd7e4940e9ee
“den987655” <den987655@gmail.com>
2024-06-04 20:14:01 +0300

    add 02

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,5 +1,5 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
-let result = (number1 % 2)
+number1 % 2 == 0
   ? console.log(`Четное число: ${number1}`)
   : console.log(`Нечетное число: ${number1}`);
