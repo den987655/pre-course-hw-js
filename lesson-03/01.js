fab88d4f1ba772c317f5d0db3945beed9526b599
“den987655” <den987655@gmail.com>
2024-06-05 13:44:29 +0300

    add 03

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,6 +1,2 @@
-// code
 let number = 1;
-console.log(number++); 
-console.log(++number); 
-console.log(number--);
-console.log(--number); 
+console.log(`${number++}, ${++number}, ${number--}, ${--number}`); 
