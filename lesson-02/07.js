e3a712b2955cafe56210d8d9dda6b12ff759852b
“den987655” <den987655@gmail.com>
2024-06-07 20:40:43 +0300

    add 2 07

---
+++ lesson-02/07.js
@@ -0,0 +1,13 @@
+let passportMarried = {
+  name: "Petr",
+  surname: "Petrov",
+  address: {
+    country: "USA",
+    city: "Bobryisk",
+  },
+};
+let passportMarried2 = {...passportMarried } 
+passportMarried2.married = true;
+ 
+console.log(passportMarried); 
+console.log(passportMarried2);
