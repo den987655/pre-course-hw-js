5cb46efda69ec20814b9e51fa9891606d4d97947
“den987655” <den987655@gmail.com>
2024-06-22 08:43:25 +0300

    add 3 01

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,15 @@
 // code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+
+function createColorString() {
+  let array = []
+    // return colors ФИЛЬТРУЕМ.ПРЕВРАЩАЕМ В СТРОКУ('-')
+    const colorFilter = colors.filter((item) => {
+     if(item === "черный" || item === "красный" || item === "желтый") 
+        array.push(item)
+    });
+    return array.join();
+}
+
+console.log(createColorString());
